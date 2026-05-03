152ed68857332038b77edbd5b7d746c262ac9485
Karthik Pai <karthikpai282@gmail.com>
2026-05-03 14:13:30 +0530

    update -> char count with & without space

--- Lab/SentenceCount.java
+++ Lab/SentenceCount.java
@@ -2,9 +2,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-
-//4. Write a Java program that analyzes a given text string and provides statistics on characters, words,
-//and sentences.
 class SentenceCount{
     String msg;
     
@@ -12,8 +9,8 @@ class SentenceCount{
         this.msg=msg;
     }
     
-    byte wordCount(){
-    byte w=0;
+    short wordCount(){
+    short w=0;
     
     for(int i=0;i<msg.length();i++)
     {
@@ -21,12 +18,14 @@ class SentenceCount{
         if(ch==' ')
              w++;
     }
+    if(msg.length()>0){
+      w++;
+    }
     return w;
-    
 }
 
- short sentenceCount(){
-    short s=0;
+ byte sentenceCount(){
+    byte s=0;
     
     for(int i=0;i<msg.length();i++)
     {
@@ -41,25 +40,38 @@ class SentenceCount{
  
  int charCount()
  {
-     return (msg.length());
+    int c=0;
+    for(int i=0;i<msg.length();i++)
+    {
+        char ch=msg.charAt(i);
+        if(ch!=' ')
+             c++;
+         
+    }
+    return c;
  }
  
  void display()
  {
      System.out.println("Word Count is:"+wordCount());
      System.out.println("Sentence Count is:"+sentenceCount());
-     System.out.println("Character Count is:"+charCount());
+     System.out.println("Character Count Without Space is:"+charCount());
+     System.out.println("Character Count With Space is:"+msg.length());
  }
 }
 
 
 
-public class SentenceMain {
+public class PracticePython {
 
     public static void main(String[] args) {
         String msg=args[0];
-        SentenceCount count= new SentenceCount(msg);
-        count.display();
+        SentenceCount count1= new SentenceCount(msg);
+        SentenceCount count2 = new SentenceCount(args[1]);
+        SentenceCount count3 = new SentenceCount(args[2]);
+        count1.display();
+        count2.display();
+        count3.display();
                 
     }
     
