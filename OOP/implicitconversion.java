d29aa79cbf8ae7cc2f79592f4e003827cf9d6933
Karthik <karthikpai282@gmail.com>
2026-07-25 18:28:16 +0530

    Implicit conversion

---
+++ OOP/implicitconversion.java
@@ -0,0 +1,21 @@
+class Sample {
+
+
+    public static void main(String[] args)
+    {
+        int i = 100;
+
+        // Automatic type conversion
+        // Integer to long type
+        long l = i;
+
+        // Automatic type conversion
+        // long to float type
+        float f = l;
+
+  
+        System.out.println("Int value " + i);
+        System.out.println("Long value " + l);
+        System.out.println("Float value " + f);
+    }
+}
