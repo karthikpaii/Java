2b9785f6e94e31fe5436420a97082e5b4492c4e6
Karthik <karthikpai282@gmail.com>
2026-08-26 11:06:34 +0530

    Add arraySample.java with array initialization and loop

---
+++ l&T Training/arraySample.java
@@ -0,0 +1,13 @@
+public class arraySample {
+    public static void main(String[] args) {
+        int arr[]= new int[5];
+
+        arr[0]=5;
+
+        for(int i=0;i<arr.length;i++){
+            System.out.println(arr[i]);
+
+        }
+    }
+    
+}
