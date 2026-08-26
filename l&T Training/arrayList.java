080db518d037dba04dd0dd334571804718ee240d
Karthik <karthikpai282@gmail.com>
2026-08-26 11:06:54 +0530

    Add arrayList.java to demonstrate ArrayList usage

---
+++ l&T Training/arrayList.java
@@ -0,0 +1,19 @@
+
+import java.util.ArrayList;
+
+
+public class arrayListt {
+    public static void main(String[] args) {
+        
+    ArrayList<Integer> arr= new ArrayList<>();
+    arr.add(20);
+    arr.add(30);
+    arr.add(40);
+    arr.add(50);
+
+    System.out.println(arr);
+    System.out.println(arr.size());
+    System.err.println(arr.get(3));
+
+    }
+}
