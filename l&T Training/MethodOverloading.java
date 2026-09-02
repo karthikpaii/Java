7108d97b7e36e9db8ce39077b6eea0cc500efd71
Karthik <karthikpai282@gmail.com>
2026-09-02 18:23:59 +0530

    method overloading

---
+++ l&T Training/MethodOverloading.java
@@ -0,0 +1,22 @@
+package com.prathik;
+
+public class MethodOverloading {
+	
+
+
+	public static void main(String[] args) {
+		System.out.println("Hello, World.");
+		main();
+		main(10);
+		// TODO Auto-generated method stub
+	}
+	public static void main () {
+		System.out.println("I'm");
+	}
+	
+	public static void main (int a) {
+		System.out.println("Prathik");
+	}
+
+
+}
